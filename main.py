00f869c7877fa337b1305000c5725e35c0479abf
nuthan kumar <nuthan.kumar@expanz.in>
2023-11-19 08:59:00 +0530

    instinal commit

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from src.models.wrapper import DIETClassifierWrapper
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+config_file = "src/config.yml"
+wrapper = DIETClassifierWrapper(config=config_file)
 
+#predict
+wrapper.predict(["How to check attendance?"])
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#train
+#after training, wrapper will load best model automatically
+wrapper.train_model(save_folder="test_model")
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
